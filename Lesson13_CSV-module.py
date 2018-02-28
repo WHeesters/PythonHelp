7aa26f7ff64c5f8c0d327095ae54f3cdea539bee
WHeesters <wheesters95@gmail.com>
2018-02-28 15:56:06 +0100

    Added lesson 13

--- Lesson13_CSV-module.py
+++ Lesson13_CSV-module.py
@@ -18,3 +18,34 @@ with open('files/names.csv', 'r') as csv_file:
         for line in csv_reader:
             print(line)  # Every line is a list with all values
             csv_writer.writerow(line)
+
+print("")
+print("")
+print("================================================")
+print("DICT MODULE")
+print("================================================")
+
+with open('files/names.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    for line in csv_reader:
+        print(line)  # Prints every line as an ordered dictionary and first line does not use field names
+
+with open('files/names.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    print("")
+    for line in csv_reader:
+        print(line['email'])  # Prints every value at the 'email' key
+
+with open('files/names.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    with open('files/new_names_dict.csv', 'w') as new_file:
+        fieldnames = ['first_name', 'email']  # Choose fieldnames
+        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\t')
+
+        csv_writer.writeheader()  # Writes the header for the csv file
+
+        for line in csv_reader:
+            del line['last_name']  # Deletes the value at given key
+            csv_writer.writerow(line)
